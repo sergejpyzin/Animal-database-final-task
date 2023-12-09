deb1af3b9179a08018c3e1109a96b486b6c47aa1
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-09 10:46:57 +0300

    created Cat.java

--- src/main/java/Models/Cat.java
+++ src/main/java/Models/Cat.java
@@ -1,4 +1,19 @@
 package Models;
 
-public class Cat {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cat extends Pets{
+    private String animalName;
+    private String animalType;
+    private  String birthdate;
+    private String commands;
+
+
 }

6bdf429ab6afe1ec54926ed7a480f99ce03379d1
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-09 10:06:40 +0300

    finished CreateTables.java

--- src/main/java/DatabaseLayer/CreateTables.java
+++ src/main/java/DatabaseLayer/CreateTables.java
@@ -1,4 +1,32 @@
 package DatabaseLayer;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateTables {
+
+    public static void cteatedTable(){
+        try(Connection connection = DataBaseConnection.getConnection()) {
+            Statement statement = connection.createStatement();
+
+            String createTablePets = "CREATE TABLE IF NOT EXISTS Pets (`pet_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL, " +
+                    "`name_pet` VARCHAR (25), " +
+                    "`animal_type` VARCHAR (25), " +
+                    "`birthdate` DATE), " +
+                    "Commands VARCHAR(100))";
+            statement.executeUpdate(createTablePets);
+
+            String createTablePackAnimals = "CREATE TABLE IF NOT EXISTS Pack_Animals (ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL, " +
+                    "Name VARCHAR(25), " +
+                    "Type VARCHAR(25), " +
+                    "BirthDate DATE, " +
+                    "Commands VARCHAR(100))";
+            statement.executeUpdate(createTablePackAnimals);
+
+            System.out.println("Таблицы Pets и Pack_Animals созданы успешно!");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при создании таблиц: " + e.getMessage());
+        }
+    }
 }

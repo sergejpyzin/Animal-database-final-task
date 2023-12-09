a70cdd3f9efadad4c29786b607e24b1cffefa5e1
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-09 10:39:54 +0300

    changed Pets

--- src/main/java/Models/Pets.java
+++ src/main/java/Models/Pets.java
@@ -1,4 +1,41 @@
 package Models;
 
-public class Pets {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pets implements Animals{
+
+    private int id;
+    private String animalName;
+    private String animalType;
+    private String birthdate;
+    private String commands;
+    @Override
+    public int getID() {
+        return id;
+    }
+
+    @Override
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    @Override
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    @Override
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    @Override
+    public String getCommands() {
+        return commands;
+    }
 }

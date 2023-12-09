2f6785995d136c11ee589c98274f767e87ce2bcd
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-12-09 09:27:24 +0300

    final created CreateDatabase.java

--- src/main/java/DatabaseLayer/CreateDatabase.java
+++ src/main/java/DatabaseLayer/CreateDatabase.java
@@ -1,4 +1,20 @@
 package DatabaseLayer;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateDatabase {
+
+    public static CreateDatabase createDatabase() {
+        try (Connection conn = DataBaseConnection.getConnection()) {
+            Statement statement = conn.createStatement();
+            String sql = "CREATE DATABASE IF NOT EXISTS human_friends";
+            statement.executeUpdate(sql);
+            System.out.println("База данных human_friends создана успешно!");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при создании базы данных: " + e.getMessage());
+        }
+        return null;
+    }
 }
